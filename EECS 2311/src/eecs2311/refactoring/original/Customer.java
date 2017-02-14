a606bed16be2e99f69b459612430512d7aef86e0
biltzerpos <bil@cse.yorku.ca>
2017-02-14 13:37:40 -0500

    Removed warnings

--- EECS 2311/src/eecs2311/refactoring/original/Customer.java
+++ EECS 2311/src/eecs2311/refactoring/original/Customer.java
@@ -21,7 +21,7 @@ public class Customer {
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
-		Enumeration rentals = _rentals.elements();
+		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()){
 			double thisAmount = 0;
